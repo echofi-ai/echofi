49f092aed254e6ac535fe81a5e1dfb8250f14218
Đông Liều <duongduydongtq@gmail.com>
2025-06-11 11:42:37 +0700

    update script tcp default

--- script/run-node.sh
+++ script/run-node.sh
@@ -23,20 +23,20 @@ echofid genesis collect-gentxs
 # Run this to ensure everything worked and that the genesis file is setup correctly
 echofid genesis validate-genesis
 
-# validator2
-VALIDATOR2_CONFIG=$HOME/.echofi/config/config.toml
-sed -i -E 's|tcp://127.0.0.1:26658|tcp://127.0.0.1:26655|g' $VALIDATOR2_CONFIG
-sed -i -E 's|tcp://127.0.0.1:26657|tcp://127.0.0.1:26654|g' $VALIDATOR2_CONFIG
-sed -i -E 's|tcp://0.0.0.0:26656|tcp://0.0.0.0:26653|g' $VALIDATOR2_CONFIG
-sed -i -E 's|allow_duplicate_ip = false|allow_duplicate_ip = true|g' $VALIDATOR2_CONFIG
-sed -i -E 's|prometheus = false|prometheus = true|g' $VALIDATOR2_CONFIG
-sed -i -E 's|prometheus_listen_addr = ":26660"|prometheus_listen_addr = ":26630"|g' $VALIDATOR2_CONFIG
+# # validator2
+# VALIDATOR2_CONFIG=$HOME/.echofi/config/config.toml
+# sed -i -E 's|tcp://127.0.0.1:26658|tcp://127.0.0.1:26655|g' $VALIDATOR2_CONFIG
+# sed -i -E 's|tcp://127.0.0.1:26657|tcp://127.0.0.1:26654|g' $VALIDATOR2_CONFIG
+# sed -i -E 's|tcp://0.0.0.0:26656|tcp://0.0.0.0:26653|g' $VALIDATOR2_CONFIG
+# sed -i -E 's|allow_duplicate_ip = false|allow_duplicate_ip = true|g' $VALIDATOR2_CONFIG
+# sed -i -E 's|prometheus = false|prometheus = true|g' $VALIDATOR2_CONFIG
+# sed -i -E 's|prometheus_listen_addr = ":26660"|prometheus_listen_addr = ":26630"|g' $VALIDATOR2_CONFIG
 
-VALIDATOR2_APP_TOML=$HOME/.echofi/config/app.toml
-sed -i -E 's|tcp://localhost:1317|tcp://localhost:1316|g' $VALIDATOR2_APP_TOML
-sed -i -E 's|localhost:9090|localhost:9088|g' $VALIDATOR2_APP_TOML
-sed -i -E 's|localhost:9091|localhost:9089|g' $VALIDATOR2_APP_TOML
-sed -i -E 's|tcp://0.0.0.0:10337|tcp://0.0.0.0:10347|g' $VALIDATOR2_APP_TOML
+# VALIDATOR2_APP_TOML=$HOME/.echofi/config/app.toml
+# sed -i -E 's|tcp://localhost:1317|tcp://localhost:1316|g' $VALIDATOR2_APP_TOML
+# sed -i -E 's|localhost:9090|localhost:9088|g' $VALIDATOR2_APP_TOML
+# sed -i -E 's|localhost:9091|localhost:9089|g' $VALIDATOR2_APP_TOML
+# sed -i -E 's|tcp://0.0.0.0:10337|tcp://0.0.0.0:10347|g' $VALIDATOR2_APP_TOML
 
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
 echofid start --pruning=nothing  --minimum-gas-prices=0.0001stake
